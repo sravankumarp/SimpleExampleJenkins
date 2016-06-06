a60c35065f1d46b8157099d8f1f9dcb390e0d598
Sravan Kumar P <sravan@sravans-sia-imac.local>
2016-06-06 18:39:17 +0530

    Initial Commit

---
+++ SimpleExampleJenkins/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleExampleJenkins
+//
+//  Created by Sravan on 06/06/16.
+//  Copyright © 2016 Sravan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleExampleJenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleExampleJenkins
+//
+//  Created by Sravan on 06/06/16.
+//  Copyright © 2016 Sravan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
