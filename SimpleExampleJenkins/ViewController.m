ed8bf82ece8418c67b6cb3653fed4606a2c9c357
Sravan Kumar P <sravan@sravans-sia-imac.local>
2016-06-09 14:51:31 +0530

    junk code commit

--- SimpleExampleJenkins/ViewController.m
+++ SimpleExampleJenkins/ViewController.m
@@ -17,6 +17,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UITableView *table = [[UITableView alloc] init] ;
+    
+#warning what is the use of this warning
+    
+    
+#warning a dead condition here
+    if(3 % 2 == 4) {
+        UIView *view = [[UIView alloc] init] ;
+    } else {
+        //nothing implemented here
+    }
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
